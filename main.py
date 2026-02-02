7b946e0510a60177de2945a1629a2c8e8aaffcd7
GuyD <damariguy7@gmail.com>
2026-02-02 15:47:36 +0200

    EKF config work for static simulation

--- main.py
+++ main.py
@@ -33,26 +33,26 @@ DVL_config = {
 LC_KF_config = {
 
     # Initial attitude uncertainty per axis (deg, converted to rad)
-    'init_att_unc': np.deg2rad(0.1),
+    'init_att_unc': np.deg2rad(0.2),
 
     # Initial velocity uncertainty per axis (m/s)
-    'init_vel_unc': 2.0,
+    'init_vel_unc': 3.0,
 
     # Initial position uncertainty per axis (m)
     'init_pos_unc': 0.01,
 
     # Initial accelerometer bias uncertainty per instrument (micro-g, converted to m/s^2)
-    'init_b_a_unc': 10 * micro_g_to_meters_per_second_squared,
+    'init_b_a_unc': 100 * micro_g_to_meters_per_second_squared,
 
     # Initial gyro bias uncertainty per instrument (deg/hour, converted to rad/sec)
-    'init_b_g_unc': 0.01 * deg_to_rad / 3600,
+    'init_b_g_unc': 10 * deg_to_rad / 3600,
 
     # Moderate process noise (despite zero in simulation)
-    'gyro_noise_PSD': (0.005 * deg_to_rad / 60) ** 2,
-    'accel_noise_PSD': (0.001 * micro_g_to_meters_per_second_squared) ** 2,
+    'gyro_noise_PSD': (0.05 * deg_to_rad / 60) ** 2,
+    'accel_noise_PSD': (1.0 * micro_g_to_meters_per_second_squared) ** 2,
 
     # Accelerometer bias random walk PSD (m^2 s^-5)
-    'accel_bias_PSD': 1.0e-8,
+    'accel_bias_PSD': 1.0e-7,
 
     # Gyro bias random walk PSD (rad^2 s^-3)
     'gyro_bias_PSD': 2.0e-10,
@@ -61,7 +61,7 @@ LC_KF_config = {
     # 'pos_meas_SD': 0.5,
 
     # Velocity measurement noise SD per axis (m/s)
-    'vel_meas_SD': 0.5
+    'vel_meas_SD': 2.0
 }
 
 
@@ -148,7 +148,7 @@ if __name__ == '__main__':
     user_config = {
         'data_path': "C:\\Users\\damar\\PycharmProjects\\EKF_CalibrateNet",
         'data_type': 'sim',  # "sim" or "real"
-        'simulated_data_file_name': 'straight', #'static', 'straight'
+        'simulated_data_file_name': 'lawn_mower_50', #'static', 'straight', 'lawn_mower_50'
         'real_data_trajectory_index': '1',  # 1-13
         'train_model': False,
         'test_model': False,
